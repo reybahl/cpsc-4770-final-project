677be90ca4a6fdb2f1c5cbba6e673a51a252ecc7
reybahl <bahl.reyansh@gmail.com>
2026-03-09 23:14:53 -0400

    lint fix

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -2,7 +2,12 @@ import { drizzle } from "drizzle-orm/node-postgres";
 
 import * as schema from "./schema";
 
-export const db = drizzle(process.env.POSTGRES_URL!, {
+const connectionString = process.env.POSTGRES_URL;
+if (!connectionString) {
+  throw new Error("Missing POSTGRES_URL environment variable");
+}
+
+export const db = drizzle(connectionString, {
   schema,
   casing: "snake_case",
 });
